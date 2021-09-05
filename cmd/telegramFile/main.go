f5a927aff97ff59777c93c088be48889d1462fcb
Roger Coll <rogercoll@protonmail.com>
2021-09-05 15:26:03 +0200

    feat: add bck method

--- cmd/telegramFile/main.go
+++ cmd/telegramFile/main.go
@@ -1,17 +1,61 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
+	"time"
 
 	"github.com/rogercoll/go-dictionary"
 )
+
+// Copy the src file to dst. Any existing file will be overwritten and will not
+// copy file attributes.
+func copy(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+	return out.Close()
+}
 
 func main() {
 	dbdir, ok := os.LookupEnv("FILE_DB")
 	if !ok {
 		log.Fatal("FILE_DB is not present")
 	}
+	bckdir, ok := os.LookupEnv("BACKUP_FILE")
+	if !ok {
+		log.Fatal("BACKUP_FILE is not present")
+	}
+	ticker := time.NewTicker(30 * time.Second)
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				err := copy(dbdir, bckdir)
+				if err != nil {
+					log.Println(err)
+				}
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
 	b, err := dictionary.NewFile(dbdir, 10)
 	if err != nil {
 		log.Fatal(err)
